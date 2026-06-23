c753af2003fe61c1a06f34e244df82f301da85d2
Beacon <beaconcao@gmail.com>
2026-06-23 22:57:39 +0800

    增加细节的说明

--- beacon-commons/beacon-common-web/src/main/java/com/beacon/framework/web/config/WebAutoConfiguration.java
+++ beacon-commons/beacon-common-web/src/main/java/com/beacon/framework/web/config/WebAutoConfiguration.java
@@ -81,9 +81,12 @@ public class WebAutoConfiguration implements WebMvcConfigurer {
         // 创建 CorsConfiguration 对象
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*"); // 设置访问源地址
-        config.addAllowedHeader("*"); // 设置访问源请求头
-        config.addAllowedMethod("*"); // 设置访问源请求方法
+        // 设置访问源地址
+        config.addAllowedOriginPattern("*");
+        // 设置访问源请求头
+        config.addAllowedHeader("*");
+        // 设置访问源请求方法
+        config.addAllowedMethod("*");
         // 创建 UrlBasedCorsConfigurationSource 对象
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config); // 对接口配置跨域设置
